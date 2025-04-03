a022341ee57b758becf4bea6ce745d168cbd90eb
wuxinkui <1079894635@qq.com>
2025-04-03 09:50:41 +0800

    feat: 使用strapi快速搭建Restful API

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,5 +1,5 @@
 import SidebarNoteListFilter from '@/components/SidebarNoteListFilter';
-import { getAllNotes } from '@/lib/redis';
+import { getAllNotes } from '@/lib/strapi';
 import { sleep } from '@/lib/utils'
 
 export default async function NoteList() {
